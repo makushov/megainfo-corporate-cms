c37d4a65c3ea578abb4a0d1c18e4797e034472f5
Андрей <@devour>
2015-03-18 17:21:01 +0300

    Пагинация теперь по-умолчанию закрыта от индексации

--- application/logs/cache_log-2015-03-18.php
+++ application/logs/cache_log-2015-03-18.php
@@ -4,3 +4,5 @@
 2015-03-18 16:04:49 --> All cache files deleted
 2015-03-18 16:06:34 --> All cache files deleted
 2015-03-18 16:06:34 --> All cache files deleted
+2015-03-18 16:20:40 --> All cache files deleted
+2015-03-18 16:20:40 --> All cache files deleted
